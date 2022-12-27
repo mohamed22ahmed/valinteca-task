8dbda42a4aa265760a39efae00671ba40e1d04bf
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2022-12-27 18:48:00 +0200

    create products table

--- database/migrations/2022_12_27_163222_create_products_table.php
+++ database/migrations/2022_12_27_163222_create_products_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('sku');
+            $table->string('name');
+            $table->double('price');
+            $table->string('description');
+            $table->string('main_image');
+            
             $table->timestamps();
         });
     }
